c173e95cf8abe9f80b40af076edd13c24c4f54ea
gffrancheska <gffrancheska@gmail.com>
2017-06-06 21:45:53 -0400

    5.10 Adding Some Validation

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
+# These changes will ensure that all articles 
+# have a title that is at least five characters long. 
